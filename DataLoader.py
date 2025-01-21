3700daba1cb0af4b22e4143f96c3089b97441d02
Qi-Xin <qix@andrew.cmu.edu>
2025-01-21 15:47:42 -0500

    deleted some legacy functions

--- DataLoader.py
+++ DataLoader.py
@@ -16,57 +16,66 @@ from allensdk.brain_observatory.ecephys.ecephys_project_cache import EcephysProj
 from collections import defaultdict
 from tqdm import tqdm
 
-
-class LFP:
-    def remove_padding_single(self, npadding):
-        self.lfp = self.lfp[:,npadding:-npadding,:]
-
-    def get_power_phase(self, padding_time, lowcut=20, highcut=35):
-        self.phase, self.power = utils.get_power_phase(self.lfp, npadding=int(self.fps*padding_time), lowcut=lowcut, highcut=highcut)
-
-    def get_mean_lfp(self):
-        self.mean_lfp = self.lfp.mean(axis=2)
-        self.mean_lfp = self.mean_lfp[:,:,None]
-
-    def pre_smooth(self, moving_size = None, pooling_size = None):
-        if moving_size == None:
-            default_moving_dict = {'Allen':1, 'Tobias':1, 'Simulation':1}
-            moving_size = default_moving_dict[self.source]
-        if pooling_size == None:
-            default_pooling_dict = {'Allen':5, 'Tobias':5, 'Simulation':1}
-            moving_size = default_pooling_dict[self.source]
-        self.lfp_smooth = utils.moving_average(self.lfp, pooling_size, moving_size)
-        self.mean_lfp_smooth = utils.moving_average(self.mean_lfp, pooling_size, moving_size)
-
-    def align_lfp(self):
-        temp = np.swapaxes(self.lfp, 1,2)    # Temporally convert (electrode, time, trial) -> (electrode, trial, time)
-        self.aligned_lfp = self.lfp.reshape(temp.shape[0],-1)
+class Simple_dataloader_from_spikes():
+    def __init__(self, 
+                 spikes, 
+                 num_merge=1, 
+                 npadding=0, 
+                 train_ratio=0.7,
+                 val_ratio=0.1,
+                 batch_size=32,
+                 verbose=True):
+        """
+        Input data is a list of 3D numpy array of shape (nt, nneuron, ntrial).
+        It should be high resolution data with bin size of 1 ms or so. 
+        It will be merged to lower resolution data with bin size of num_merge times bigger. 
+        """
+        self.spikes = spikes
+        self.npadding = npadding
+        self.num_merge = num_merge
+        self.batch_size = batch_size
+        self.nt, self.nneuron, self.ntrial = self.spikes.shape
         
-    def show(self, trial=0):
-        if self.source == 'Simulation':
-            plt.figure(figsize=(8, 6))
-            plt.subplot(121)
-            plt.imshow(self.gt_csd[:,:,trial], aspect='auto', cmap='bwr')
-            plt.title('True CSD')
-            plt.ylabel('Depth (microns)')
-            plt.xlabel('Time (ms)')
-            plt.subplot(122)
-            plt.imshow(self.lfp[:,:,trial], aspect='auto', cmap='bwr')
-            plt.title('LFP (noisy)')
-            plt.xlabel('Time (ms)')
-            plt.show()
-        else:
-            plt.figure(figsize=(12, 6))
-            plt.imshow(self.lfp[:,:,trial], aspect='auto', cmap='bwr')
-            plt.title('LFP')
-            plt.xlabel('Time (ms)')
-            plt.show()
+        # Get tokenized spike trains by merging time intervals
+        self.spikes_full_w_padding = np.concatenate(self.spikes, axis=1)
+        self.spikes_full_low_res = utils.change_temporal_resolution_single(
+            self.spikes_full_w_padding[:,:,self.npadding:], self.num_merge
+        )
+        # Change to tensor
+        self.spikes_full_w_padding = torch.tensor(self.spikes_full_w_padding).float()
+        self.spikes_full_low_res = torch.tensor(self.spikes_full_low_res).float()
+        
+        # Splitting data into train and test sets
+        indices = list(range(self.ntrial))
+        utils.set_seed(1)
+        np.random.shuffle(indices)
+        split1, split2 = int(train_ratio*self.ntrial), int((train_ratio+val_ratio)*self.ntrial)
+        self.train_idx = indices[:split1]
+        self.val_idx = indices[split1:split2]
+        self.test_idx = indices[split2:]
+        
+        train_dataset = torch.utils.data.TensorDataset(self.spikes_full_low_res[self.train_idx], 
+                                                    self.spikes_full[self.train_idx])
+        val_dataset = torch.utils.data.TensorDataset(self.spikes_full_low_res[self.val_idx], 
+                                                    self.spikes_full[self.val_idx])
+        test_dataset = torch.utils.data.TensorDataset(self.spikes_full_low_res[self.test_idx], 
+                                                    self.spikes_full[self.test_idx])
 
-
-class Allen_LFP(LFP):
-    def __init__(self):
-        self.x = None
-        self.t = None
+        self.train_loader = torch.utils.data.DataLoader(train_dataset, 
+                                                        batch_size=self.batch_size, 
+                                                        shuffle=True)
+        self.val_loader = torch.utils.data.DataLoader(val_dataset,
+                                                        batch_size=self.batch_size, 
+                                                        shuffle=False)
+        self.test_loader = torch.utils.data.DataLoader(test_dataset, 
+                                                        batch_size=self.batch_size, 
+                                                        shuffle=False)
+        if verbose:
+            print(f"Total trials: {self.ntrial},
+                  Batch size: {self.batch_size},
+                  Train set size: {len(train_dataset)}, 
+                  Val set size: {len(val_dataset)},
+                  Test set size: {len(test_dataset)}")
 
 
 class Allen_dataloader_multi_session():
@@ -87,12 +96,12 @@ class Allen_dataloader_multi_session():
         self.shuffle = kwargs.pop('shuffle', True)
         self.common_kwargs = kwargs
 
-        logger = logging.getLogger(__name__)
-        logger.critical(f"Total number of sessions: {len(self.session_ids)}")
-        logger.critical(f"Train ratio: {self.train_ratio}")
-        logger.critical(f"Val ratio: {self.val_ratio}")
-        logger.critical(f"Test ratio: {1-self.train_ratio-self.val_ratio}")
-        logger.critical(f"Batch size: {self.batch_size}")
+        if verbose:
+            print(f"Total sessions: {len(self.session_ids)},
+                  Batch size: {self.params['batch_size']},
+                  Train set size: {len(train_dataset)}, 
+                  Val set size: {len(val_dataset)},
+                  Test set size: {len(test_dataset)}")
         
         # Initialize session info
         self._initialize_sessions()
@@ -628,221 +637,3 @@ class Allen_dataset:
             self.mean_pupil_diam[i] = self.pupil_diam[:,i].mean()
             self.min_pupil_diam[i] = self.pupil_diam[:,i].min()
             self.max_pupil_diam[i] = self.pupil_diam[:,i].max()
-        
-    def get_lfp(self, **kwargs):
-        self.lfp = {}
-        for probe_name in self.selected_probes:
-            
-            temp_obj = Allen_LFP()
-            probe_id = self.probes[self.probes['description']==probe_name].index[0]
-            
-            lfp_data = self._session.get_lfp(probe_id)
-            trial_window = np.arange(self.start_time-self.padding,self.end_time+self.padding, 1/self.fps)
-            time_selection = np.concatenate([trial_window + t for t in self.presentation_times])
-            inds = pd.MultiIndex.from_product((self.presentation_ids, trial_window), 
-                                        names=('presentation_id', 'time_from_presentation_onset'))
-            ds = lfp_data.sel(time = time_selection, method='nearest').to_dataset(name = 'aligned_lfp')
-            ds = ds.assign(time=inds).unstack('time')
-            lfp_temp = ds['aligned_lfp'].values     # Three dimensions. e.g. (77, 540, 625). Channels, trials, times
-            lfp_temp = np.swapaxes(lfp_temp,1,2)    # Swap time and trial. e.g. (77, 625, 540). Channels, times, trials
-            try:
-                location = self._session.channels[['probe_vertical_position','probe_horizontal_position']]
-                location = location.loc[lfp_data['channel'].values].values
-                x = location[:, 0]      # LFP spatial locations , microns
-            except:
-                location = np.arange(0,lfp_temp.shape[0])*40.0
-                x = location
-
-            # temp_obj.t = np.linspace(self.start_time,self.end_time,lfp_temp.shape[1] )[:,None]
-            temp_obj.t = trial_window
-            temp_obj.x = x[:, None]
-            temp_obj.channel = lfp_data["channel"].values
-            try:
-                temp_obj.structure_acronyms, temp_obj.intervals_lfp = self._session.channel_structure_intervals(temp_obj.channel)
-            except:
-                temp_obj.structure_acronyms = np.array([np.nan], dtype=object)
-                temp_obj.intervals_lfp = np.array([ 0, lfp_temp.shape[0]])
-            temp_obj.nx, temp_obj.nt, temp_obj.ntrial, temp_obj.lfp = utils.check_and_get_size(lfp_temp)
-            temp_obj.get_mean_lfp()
-            self.lfp[probe_name] = temp_obj
-
-    def remove_padding(self, padding_time):
-        npadding = int(padding_time*self.fps)
-        for key, value in self.lfp.items():
-            value = value.remove_padding_single(npadding)
-            # self.lfp[key] = value
-
-    def get_spike_train_sparse(self):
-        self._units_pd = self._session.units[(self._session.units.probe_id == self.probe_id)]
-        self.unit_id_list = self._units_pd.index.values
-        self._channel_index = self._units_pd.loc[self.unit_id_list].channel_local_index
-        self.st_channel_id_list = []
-        self.nunit = len(self.unit_id_list)
-        self.spike_train_sparse = []
-        for i in range(self.nunit):
-            self.st_channel_id_list.append( self._session.channels[(self._session.channels.local_index == self._channel_index.values[i]) & 
-                                                (self._session.channels.probe_id == self.probe_id)].index.values[0] )
-            spike_times = self._session.spike_times[self.unit_id_list[i]]
-            self.spike_train_sparse.append([ spike_times[ np.logical_and(spike_times>t+self.start_time, spike_times<t+self.end_time) ]-t 
-                                     for t in self.presentation_times ])
-            
-        self.st_channel_id_list = np.array(self.st_channel_id_list)
-        
-    def get_spike_train(self):
-        # (nunit, nt, ntrial)
-        from scipy.sparse import csr_matrix
-        self.get_spike_train_sparse()
-
-        self.spike_train = np.zeros((self.nunit, self.nt, self.ntrial))
-        for i in range(self.nunit):
-            row = np.array([])
-            col = np.array([])
-            data = np.array([])
-            for trial in range(self.ntrial):
-                temp = self.spike_train_sparse[i][trial]
-                nspike = len(temp)
-                row = np.hstack( (row, np.array(temp)*self.fps) )
-                col = np.hstack( (col, trial*np.ones(nspike)) )
-                data = np.hstack( (data, np.ones(nspike)) )
-            
-            self.spike_train[i,:,:] = csr_matrix((data, (row, col)), shape=(self.nt, self.ntrial)).toarray()
-        self.spike_count = self.spike_train.sum(axis=(1))
-        
-    def get_pooled_spike_train(self):
-        pooled_spike_train = []
-        for i in range(self.nunit):
-            pooled_spike_train.append( np.concatenate(self.spike_train_sparse[i]) )
-        return pooled_spike_train
-    
-    def get_fr(self):
-        raise ValueError ("need to be in rCSD folder! ")
-        # import smoothing_spline
-        # fit_model = smoothing_spline.SmoothingSpline()
-        # time_line = np.arange(self.start_time,self.end_time,1.0/self.fps)
-        # eta_smooth_tuning = 1e-10
-        # f_basis, f_Omega = fit_model.construct_basis_omega(
-        #     time_line, knots=15, verbose=False)
-        
-        # self.fr = np.zeros((self.nunit, self.nt, self.ntrial))
-        # for i in range(self.nunit):
-        #     for trial in range(self.ntrial):
-        #         temp_spike_train = self.spike_train[i,:,trial]
-        #         # log_lambda_hat, beta = fit_model.poisson_regression(temp_spike_train[None,:], f_basis)
-        #         log_lambda_hat, (beta, beta_baseline, log_lambda_offset, hessian, hessian_baseline, nll) \
-        #             = fit_model.poisson_regression_smoothing_spline(
-        #                 temp_spike_train[None,:], time_line, basis=f_basis, Omega=f_Omega, constant_fit=False)
-        #         self.fr[i, :, trial] = np.exp(log_lambda_hat)
-
-    def get_kernel_fr(self, bandwidth=0.03):
-        self.bandwidth = bandwidth
-        from sklearn.neighbors import KernelDensity
-
-        emp_fr_X = np.arange(self.start_time,self.end_time, 1/self.fps)
-        emp_fr_X = emp_fr_X[:,None]
-        emp_fr = np.zeros((self.nunit, emp_fr_X.shape[0], self.ntrial))
-        for i in range(self.nunit):
-            for trial in range(self.ntrial):
-                points = np.array(self.spike_train_sparse[i][trial])
-                
-                if len(points)>0:
-                    points = points[:,None]
-                    kde = KernelDensity(kernel='gaussian', bandwidth=bandwidth).fit(points)
-                    logdensity = kde.score_samples( emp_fr_X )
-                    emp_fr[i,:, trial] = np.exp(logdensity)
-                    total_spike = points.shape[0]
-                    emp_fr[i,:, trial] = emp_fr[i,:, trial]*total_spike
-        self.kernel_fr = emp_fr
-        return emp_fr
-
-    def get_psth(self, bandwidth=0.02):
-        self.bandwidth = bandwidth
-        from sklearn.neighbors import KernelDensity
-        pooled_spike_train = self.get_pooled_spike_train() 
-        emp_fr_X = np.arange(self.start_time,self.end_time, 1/self.fps)
-        emp_fr_X = emp_fr_X[:,None]
-        emp_fr = np.zeros((self.nunit, emp_fr_X.shape[0]))
-        for i in range(self.nunit):
-            points = np.array(pooled_spike_train[i])
-            points = points[:,None]
-            kde = KernelDensity(kernel='gaussian', bandwidth=bandwidth).fit(points)
-            logdensity = kde.score_samples( emp_fr_X )
-            emp_fr[i,:] = np.exp(logdensity)
-            total_spike = points.shape[0]
-            emp_fr[i,:] = emp_fr[i,:]*total_spike/self.ntrial
-        self.emp_fr = emp_fr
-        return emp_fr
-    
-    def print_session_info(self):
-        """Print a list of information for the session."""
-        if self.session is None:
-            print('session is None')
-            return
-
-        print(self.session._metadata)
-        print('num units:', self.session.num_units)
-
-class Tobias_LFP(LFP):
-    def __init__(self, **kwargs):
-        self.source = "Tobias"
-        self.dataset_id = kwargs.pop('dataset_id', 'Walter_20160512')
-        self.probe_id = kwargs.pop('probe_id', 805008600)
-        self.start_time = kwargs.pop('start_time', 0)
-        self.end_time = kwargs.pop('end_time', self.start_time+500)
-        self.get_Tobias()
-        
-    def get_lfp(self):
-        import scipy.io
-        mat = scipy.io.loadmat("D:/LFP data/"+self.dataset_id+".mat")
-        self.lfp = mat['data']
-        self.lfp = self.lfp[:, self.start_time: self.end_time]
-        # Set up spatial locations , temporal locations
-        self.t = np. linspace(0, self.nt, self.nt)[:, None]       # time points , milliseconds
-        self.x = np. linspace(0, 2300, 24)[:, None]     # LFP spatial locations , microns
-        self.nx, self.nt, self.ntrial, self.lfp = utils.check_and_get_size(self.lfp)
-        self.get_mean_lfp()
-        self.intervals_lfp = np.array([0, np.nonzero(self.x > 1000)[0][0],
-                                np.nonzero(self.x > 1500)[0][0], self.nx])
-        self.structure_acronyms = np.array(['Superficial', 'Medium', 'Deep'])
-    
-# class Simulation_LFP(LFP):
-#     def __init__(self, **kwargs):
-#         """To be done later. Need to include: gaussian bumps; the cases from jupyter notebooks"""
-#         if sys.platform == 'linux':
-#             sys.path.append("/home/qix/rCSD")
-#         else:
-#             sys.path.append("D:/Github/rCSD")
-#         import ground_true_csd_bank
-#         from forward_models import b_fwd_1d, fwd_model_1d
-        
-#         self.source = "Simulation"
-#         self.noise_amp = kwargs.pop('noise_amp', 0.03)
-#         if 'gt_csd' in kwargs:
-#             print("not finished! Can't allow user self design gt csd at the moment! ")
-#             raise ValueError("Unfinished!")
-#             # self.gt_csd = kwargs['gt_csd']
-#             # self.generate_lfp(noise_amp)
-#         else:
-#             self.gt_csd_id = kwargs.pop('gt_csd_id', 0)
-#             self.get_csd()
-#             self.get_lfp()
-
-#     def get_csd(self):
-
-#         self.R = 150
-#         self.nx = 24
-#         self.nz = 5*23
-#         self.x = np. linspace(0, 2300, self.nx)[:, None]
-#         self.z = np. linspace(0+10, 2300-10, self.nz)[:, None]
-#         self.gt_csd, self.t, self.nt = ground_true_csd_bank.csd_simple_templete(self.gt_csd_id, self.z)
-#         self.ntrial = self.gt_csd.shape[2]
-        
-#     def get_lfp(self):
-#         lfp_noiseless = fwd_model_1d(self.gt_csd, self.z, self.x, self.R)
-#         noise = self.noise_amp * np.abs(lfp_noiseless).max()*np.random.randn(self.nx, self.nt, self.ntrial)
-#         self.lfp = lfp_noiseless + noise
-#         self.nx, self.nt, self.ntrial, self.lfp = utils.check_and_get_size(self.lfp)
-#         self.get_mean_lfp()
-#         self.intervals_lfp = np.array([0, np.nonzero(self.x > 1000)[0][0],
-#                                 np.nonzero(self.x > 1500)[0][0], self.nx])
-#         self.structure_acronyms = np.array(['Superficial', 'Medium', 'Deep'])
-
