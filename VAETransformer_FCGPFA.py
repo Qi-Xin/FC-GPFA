b862d4dfc4f69953b214e3127dfa016066b38afc
Qi-Xin <qix@andrew.cmu.edu>
2025-03-08 21:05:37 +0000

    corrected fucking bug produced by Cursor!

--- VAETransformer_FCGPFA.py
+++ VAETransformer_FCGPFA.py
@@ -224,13 +224,7 @@ class VAETransformer_FCGPFA(nn.Module):
         coupling_filters = self.coupling_filters_dict[current_session_id]
         cp_weight_sending = self.cp_weight_sending_dict[current_session_id]
         cp_weight_receiving = self.cp_weight_receiving_dict[current_session_id]
-        
-        # Add check for spike_trains dimensions
-        spike_trains = src["spike_trains"]
-        if spike_trains.dim() == 3:
-            # Ensure dimensions are (batch, neurons, time)
-            if spike_trains.shape[0] < spike_trains.shape[2]:
-                spike_trains = spike_trains.transpose(2, 1, 0)
+        spike_trains = src["spike_trains"].permute(2,1,0)
 
         for jarea in range(self.narea):
             for iarea in range(self.narea):
